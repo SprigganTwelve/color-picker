7924398765d1ed68a08b43a3f8ce3586ea755194
Paterne <angepaterne.dali@gmail.com>
2023-12-27 18:02:43 +0100

    structure

--- js.js
+++ js.js
@@ -7,6 +7,13 @@ var hexCopy = document.querySelector("#hexCopy");
 var rgbCopy = document.querySelector("#rgbCopy");
 var input = document.querySelector("input");
 var img = document.querySelector("img");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+if ((body.style.color = "white")) {
+} else {
+  h1.style.color = "white";
+}
 
 colorPickUp.addEventListener("click", function () {
   var color = new EyeDropper();
@@ -14,18 +21,19 @@ colorPickUp.addEventListener("click", function () {
     console.log(result);
     var hexColor = result.sRGBHex;
     div.style.background = `${hexColor}`;
-    hex.textContent = `${hexColor}`;
+    hex.textContent = ` ${hexColor} `;
     var r = parseInt(hexColor.substring(1, 3), 16);
     var g = parseInt(hexColor.substring(3, 5), 16);
     var b = parseInt(hexColor.substring(5, 7), 16);
-    rgb.textContent = `rgb(${r},${g},${b})`;
+    rgb.textContent = `  rgb(${r},${g},${b}) `;
   });
 });
+var copy;
 hexCopy.addEventListener("click", () => {
-  navigator.clipboard.writeText(hex.textContent).then(alert("text copié"));
+  copy = navigator.clipboard.writeText(hex.textContent).then(alert("text copié"));
 });
 rgbCopy.addEventListener("click", () => {
-  navigator.clipboard.writeText(rgb.textContent).then(alert("text copié"));
+  copy = navigator.clipboard.writeText(rgb.textContent).then(alert("text copié"));
 });
 
 input.addEventListener("change", () => {
